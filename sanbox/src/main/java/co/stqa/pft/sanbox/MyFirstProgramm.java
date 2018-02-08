463984a61dade4fe550f58a8875c1178204b9615
Natan Radostin <natanbig@gmail.com>
2018-02-08 15:33:15 +0200

    Changed format settings

--- sanbox/src/main/java/co/stqa/pft/sanbox/MyFirstProgramm.java
+++ sanbox/src/main/java/co/stqa/pft/sanbox/MyFirstProgramm.java
@@ -1,8 +1,7 @@
 package co.stqa.pft.sanbox;
 
 public class MyFirstProgramm {
-
-	public static void main(String[] args){
-		System.out.println("Hello World");
-	}
+  public static void main(String[] args) {
+    System.out.println("Hello World");
+  }
 }
